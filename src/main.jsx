3730b5bb13e94d70fe3bfbec91699c8848d6cc5a
Thakran <prashantthakran0664@gmail.com>
2023-02-22 21:38:44 +0530

    Aon

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { inject } from '@vercel/analytics';
 
+inject();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
